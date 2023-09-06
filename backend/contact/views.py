f207fd467ba10138cb51c470f2a55becd1583c55
Erhan <erhan.citil@maykinmedia.nl>
2023-09-06 10:46:59 +0200

    🎨 Contact views

--- backend/contact/views.py
+++ backend/contact/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import FormView
+from .forms import ContactFormulier
 # Create your views here.
+class ContactFormView(FormView):
+    template_name = 'contact.html'
+    form_class = ContactFormulier
+    success_url = '/contact/'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
