49e34d33a6e1cdae78a8d84fbfcc00545226a418
Erhan <erhan.citil@maykinmedia.nl>
2023-09-06 10:22:41 +0200

    🐛 Fixed migrations issue

--- backend/devworld/settings.py
+++ backend/devworld/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
-    "django.contrib.apps"
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
@@ -124,3 +123,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = 'members.CustomUser'
