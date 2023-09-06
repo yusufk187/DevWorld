7cd84cc5f1d895dd9bfac1f28faf32be3a00074a
Erhan <erhan.citil@maykinmedia.nl>
2023-09-06 11:37:32 +0200

    Added AUTH USER MODEL

--- backend/devworld/settings.py
+++ backend/devworld/settings.py
@@ -124,9 +124,3 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = 'members.CustomUser'
-AUTH_USER_MODEL = 'appname.CustomUser'
-
-LOGIN_URL = 'login'
-LOGOUT_URL = 'logout'
-
-
