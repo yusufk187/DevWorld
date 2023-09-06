4d98c127f9fca80b190b275f0a45d5d9f62298b2
Erhan <erhan.citil@maykinmedia.nl>
2023-09-06 10:15:56 +0200

    🎨 Moved templates folder to backend folder

--- backend/devworld/settings.py
+++ backend/devworld/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "devworld.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
