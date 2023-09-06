e7e34706068e17df4ffcae0c59e5134efc5c1095
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2023-09-06 11:36:27 +0200

    Batus

--- backend/devworld/members/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('members', views.index, name='index'),
-]
--- backend/devworld/settings.py
+++ backend/devworld/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "django.contri|b.sessions.middleware.SessionMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
